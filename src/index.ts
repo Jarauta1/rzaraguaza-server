ddde2f000dbc8928e0a34ef06f7addd52f385873
Jarauta1 <diegojarauta8@gmail.com>
2021-11-29 23:10:34 +0100

    add function

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import express, { Application } from 'express';
 import cors from 'cors';
 import routes from './app-routes/app.routes';
+import { nextTick } from 'process';
 
 const app: Application = express();
 app.use(cors());
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Acces-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 
 app.get('/', (req, res) => {
     res.send('hola')
